2ac8b446d50822571a9951114d92797c47f8c60d
Anton Chebanash <achebanash@andiks.com>
2018-07-25 13:14:23 +0300

    added rest method

--- app.js
+++ app.js
@@ -1,54 +1,92 @@
 "use strict"
-// This is the server-side file of our mobile remote controller app.
-// It initializes socket.io and a new express instance.
-// Start it by running 'node app.js' from your terminal.
-
-
-// Creating an express server
-
 var express = require('express'),
     app = express();
 var bodyParser = require('body-parser');
-// This is needed if the app is run on heroku and other cloud providers:
-
 var port = process.env.PORT || 5000;
-
-// Initialize a new socket.io object. It is bound to 
-// the express app, which allows them to coexist.
-
 var io = require('socket.io').listen(app.listen(port));
-
-
-// App Configuration
-
-// Make the files in the public folder available to the world
 app.use(express.static(__dirname + '/public'));
-
-// This is a secret key that prevents others from opening your presentation
-// and controlling it. Change it to something that only you know.
-
 var socketList = [];
 var id;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.render('index.ejs', {});
+
 });
 
-app.get('/candidateTable.ejs', function (req, res) {
-    // var conString = "postgres://postgres:Radius123@10.0.0.4:5432/hh_resume";
-    var conString = "postgres://postgres:Radius123@109.188.79.65:5432/hh_resume";
-    var pg = require('pg');
-    var client = new pg.Client(conString);
-    client.connect();
-    const query = client.query('SELECT json_agg(*) FROM public.resume', (err, result) => {
-        res.render('candidateTable.ejs', {candidates: result.rows[0].json_agg});
-    })
+const {Pool} = require('pg');
+const connectionString = 'postgres://postgres:Radius123@109.188.79.65:5432/hh_resume';
+
+const pool = new Pool({
+    connectionString: connectionString,
 });
-app.get('')
-app.get('/profile.ejs', function (req, res) {
-    res.render('profile.ejs', {candidates: null});
+
+app.post('/setComment', function (req, res) {
+
+});
+
+app.get('/profile', function (req, res) {
+    var first = true;
+    var limit = false;
+    var offset = false;
+    var limitCount = 0;
+    var offsetCount = 0;
+    var query = "SELECT * FROM public.resume";
+    if (Object.keys(req.query).length != 0) {
+        query += " WHERE ";
+        if (req.query.hasOwnProperty("from_date") && req.query.hasOwnProperty("to_date")) {
+            query += "date_load BETWEEN " + "'" + req.query['from_date'] + "'" + " AND " + "'" + req.query['to_date'] + "'";
+            delete req.query['from_date'];
+            delete req.query['to_date'];
+            first = false;
+        }
+        if (req.query.hasOwnProperty("last_update_from_date") && req.query.hasOwnProperty("last_update_to_date")) {
+            query += "last_update_date BETWEEN " + "'" + req.query['last_update_from_date'] + "'" + " AND " + "'" + req.query['last_update_to_date'] + "'";
+            delete req.query['last_update_from_date'];
+            delete req.query['last_update_to_date'];
+            first = false;
+        }
+        if (req.query.hasOwnProperty("offset")) {
+            offset = true;
+            offsetCount = req.query['offset'];
+            delete req.query['offset'];
+        }
+        if (req.query.hasOwnProperty("limit")) {
+            limit = true;
+            limitCount = req.query['limit'];
+            delete req.query['limit'];
+        }
+        for (var key in req.query) {
+            if (key == 'key_level' || key == 'name' || key == 'gender' || key == 'm_comment' || key == 'lavel_year' || key == 'age' || key == 'salary') {
+                if (first) {
+                    query += key + " LIKE '%" + req.query[key] + "%'";
+                    first = false;
+                } else {
+                    query += " AND " + key + " LIKE '%" + req.query[key] + "%'";
+                }
+            } else {
+                if (first) {
+                    query += key + "=" + req.query[key] + " ";
+                    first = false;
+                } else {
+                    query += " AND " + key + "=" + req.query[key] + " ";
+                }
+            }
+        }
+        if (limit) {
+            query += "limit " + limitCount + " ";
+        }
+        if (offset) {
+            query += "offset " + offsetCount + " ";
+        }
+    }
+    pool.query(query, (err, result) => {
+        if(typeof result.rows != "undefined"){
+            res.send(result.rows);
+        } else {
+            res.send("NOTHING FOUND");
+        }
+    })
 });
 
 // Initialize a new socket.io application
@@ -56,18 +94,12 @@ var presentation = io.on('connection', function (socket) {
     console.log("client connected: " + socket.id);
     socket.emit('checkIfAndroid', true);
     socket.on('checkIfAndroid', function (flag) {
-        if(flag == true){
+        if (flag == true) {
             socketList.push(socket.id);
         }
     });
     socket.on('getNextPhone', function (count) {
-        var conString = "postgres://postgres:Radius123@109.188.79.65:5432/hh_resume";
-        // var conString = "postgres://postgres:Radius123@10.0.0.4:5432/hh_resume";
-        var pg = require('pg');
-        var client = new pg.Client(conString);
-        client.connect();
-        const query = client.query('SELECT * FROM public.resume WHERE w_sign = 0', (err, res) => {
-            console.log(res.rows[count].name);
+        pool.query('SELECT * FROM public.resume WHERE w_sign = 0', (err, result) => {
             socket.emit('getPhone', {
                 "phoneNumber": res.rows[count].telephone,
                 "name": res.rows[count].name,
@@ -76,22 +108,20 @@ var presentation = io.on('connection', function (socket) {
                 "salary": res.rows[count].salary,
                 "skills": res.rows[count].key_level,
                 "lenght": res.rows.length
-            })
-        })
-        // client.close(); //TODO wtf how it works with promises ???? обработчик ошибок
-    });
-    socket.on('audio', function (im){
-        console.log(im);
-        console.log("ffff");
+            });
+            pool.end();
+        });
     });
 
-    socket.on('showCalling', function(incomingNumber){
-        console.log(incomingNumber.toString());
-        var conString = "postgres://postgres:Radius123@109.188.79.65:5432/hh_resume";
-        // var conString = "postgres://postgres:Radius123@10.0.0.4:5432/hh_resume";
-        var pg = require('pg');
-        var client = new pg.Client(conString);
-        client.connect();
+    // put audio in to bd
+    socket.on('audio', function (im) {
+        pool.query('SELECT * FROM public.resume WHERE w_sign = 0', (err, result) => {
+            pool.end();
+        });
+    });
+
+    //
+    socket.on('showCalling', function (incomingNumber) {
         const query = client.query('SELECT * FROM public.resume WHERE telephone = ' + incomingNumber, (err, res) => {
 
         })
@@ -130,20 +160,6 @@ var presentation = io.on('connection', function (socket) {
         } else if (buttonId.indexOf("comment") >= 0) {
             id = buttonId.substring(8);
             console.log(id);
-        }
-    });
-
-    socket.on('slide-changed', function (data) {
-
-        // Check the secret key again
-
-        if (data.key === secret) {
-
-            // Tell all connected clients to navigate to the new slide
-
-            presentation.emit('navigate', {
-                hash: data.hash
-            });
         }
     });
 });
